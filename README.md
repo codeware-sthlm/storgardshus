1f4632bbd0fb8b1a1a29c577e1edc41a2fe6ea90
Mathias Lundin <mathias.lundin@codeware.se>
2023-05-11 21:30:50 +0200

    improve seo

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Storgårdshus AB
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/94c87a62-d229-488c-b4c5-d0f3b1c70a00/deploy-status)](https://app.netlify.com/sites/fascinating-daffodil-1cd3ec/deploys)
--- src/config/consts.ts
+++ src/config/consts.ts
@@ -5,14 +5,10 @@ export const COMPANY_FULL = 'Storgårdshus AB';
 export const SLOGAN = 'Vi bygger dina drömmar'; //'Vi förverkligar dina byggdrömmar'; //'Experter inom bygg sedan 1996';
 
 export const SITE_TITLE = `${COMPANY_FULL} - ${SLOGAN}`;
-export const SITE_DESCRIPTION = `Välkommen till ${COMPANY_SHORT}, det professionella byggföretaget som verkar i Västerås och Mälardalen. Hos oss får du skräddarsydda lösningar, noggrannhet och kvalitet i varje projekt – allt för att förverkliga dina drömmar och visioner.`;
+export const SITE_DESCRIPTION = `Välkommen till ${COMPANY_SHORT}, det professionella byggföretaget som verkar i Västerås och Mälardalen.`;
 
 export const CONTACT: { address: string; mail: string; phone: string } = {
   address: 'Drottninggatan 4A, 724 64 Västerås',
   mail: 'info@storgardshus.se',
   phone: '+46701234567'
 };
-
-// Work in progressq
-// export const SITE_DESCRIPTION = `${COMPANY} är ett byggföretag som arbetar i Västerås och Mälardalen. Vi utför renoveringar, ombyggnader, tillbyggnader, nyproduktioner samt bullerskärmar åt både privatpersoner och företag i Västerås.`;
-// "Välkommen till ${COMPANY}, det professionella byggföretaget som verkar i Västerås och Mälardalen. Vi erbjuder en rad tjänster inom renoveringar, ombyggnationer, tillbyggnader, nyproduktioner och bullerskärmar för både privatpersoner och företag. Hos oss får du skräddarsydda lösningar, noggrannhet och kvalitet i varje projekt – allt för att förverkliga dina drömmar och visioner i Västerås med omnejd."
